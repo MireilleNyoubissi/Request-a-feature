b67bc575003c781967595dcc724d6bd45e503d3d
MireilleNyoubissi <nmireillemat@yahoo.fr>
2025-02-04 13:37:13 +0000

    Implemented findById method

--- src/main/java/com/requestafeature/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/requestafeature/service/impl/CompanyServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CompanyServiceImpl implements CompanyService {
@@ -26,5 +27,10 @@ public class CompanyServiceImpl implements CompanyService {
         companyRepository.save(company);
 
         return company;
+    }
+
+    @Override
+    public Optional<Company> findById(int id) {
+        return companyRepository.findById(id);
     }
 }
