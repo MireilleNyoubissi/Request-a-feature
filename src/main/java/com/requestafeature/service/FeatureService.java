b65e9b3ae2b44b10c702f485e3a3292f80716c67
MireilleNyoubissi <nmireillemat@yahoo.fr>
2025-02-04 14:55:23 +0000

    Added find all feature by company method

--- src/main/java/com/requestafeature/service/FeatureService.java
+++ src/main/java/com/requestafeature/service/FeatureService.java
@@ -3,7 +3,9 @@ package com.requestafeature.service;
 import com.requestafeature.entity.Feature;
 import com.requestafeature.payload.FeatureDto;
 
+import java.util.List;
+
 public interface FeatureService {
     Feature createFeature(FeatureDto featureDto) throws Exception;
-
+    List<Feature> findAllFeaturesByCompany(Integer companyId);
 }
